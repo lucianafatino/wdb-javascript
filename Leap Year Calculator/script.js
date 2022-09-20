f4b98683bab978f0b7355f3ae5b3c99b99eaaec5
lucianafatino <lucianavenancio54@gmail.com>
2022-09-20 10:24:52 -0300

    Leap Year Calculator Exercise

---
+++ Leap Year Calculator/script.js
@@ -0,0 +1,15 @@
+function isLeap(year) {
+  if (year % 4 !== 0) {
+    return "Not leap year.";
+  }
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return "Leap year.";
+  }
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
+    return "Not leap year.";
+  }
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return "Leap year.";
+  }
+}
+isLeap(2000);
